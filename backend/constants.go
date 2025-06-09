ebcdddab48ec3f28eea61ee416e5295055d88190
bzelaznicki <bartosz@zelaznicki.com>
2025-06-09 22:59:05 +0200

    validating JWT

--- backend/constants.go
+++ backend/constants.go
@@ -2,7 +2,7 @@ package main
 
 import "time"
 
-const CookieExpirationTime = 10 * time.Minute
+const CookieExpirationTime = 5 * time.Minute
 const TimeFormat string = "2006-01-02 15:04:05"
 const refreshTokenExpirationDays = 30
 const refreshTokenExpiration = refreshTokenExpirationDays * 24 * time.Hour
--- backend/internal/auth/token_generation.go
+++ backend/internal/auth/token_generation.go
@@ -26,6 +26,32 @@ func GenerateJWT(userId, email, tokenSecret string, isAdmin bool, tokenExpiratio
 	return signedToken, nil
 }
 
+func ValidateJWT(tokenString, tokenSecret string) (string, error) {
+	claims := jwt.MapClaims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+
+	if err != nil || !token.Valid {
+		return "", fmt.Errorf("invalid token: %v", err)
+	}
+	exp, ok := claims["exp"].(float64)
+	if !ok {
+		return "", fmt.Errorf("expiration (exp) not found in token claims")
+	}
+	if int64(exp) < time.Now().Unix() {
+		return "", fmt.Errorf("token has expired")
+	}
+
+	userId, ok := claims["user_id"].(string)
+	if !ok {
+		return "", fmt.Errorf("user_id not found in token claims")
+	}
+
+	return userId, nil
+}
+
 func MakeRefreshToken() (string, error) {
 
 	bytes := make([]byte, 32)
