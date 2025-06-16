824a30b39bd5d8358690a48a1cddec36a15df2d9
bzelaznicki <bartosz@zelaznicki.com>
2025-06-16 14:46:04 +0200

    logout change on frontend

--- frontend/src/lib/logout.ts
+++ frontend/src/lib/logout.ts
@@ -3,7 +3,7 @@ import { API_BASE_URL } from "./config";
 export async function logout() {
   localStorage.removeItem('token');
   localStorage.removeItem('user');
-  await fetch(`http://${API_BASE_URL}/api/logout`, {
+  await fetch(`${API_BASE_URL}/api/logout`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     credentials: 'include',
