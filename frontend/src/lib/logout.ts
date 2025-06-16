d3b69cb5ec7c7764bc9f96fcd3d9a83ec2697acc
bzelaznicki <bartosz@zelaznicki.com>
2025-06-16 14:19:50 +0200

    updated logout bug on frontend

--- frontend/src/lib/logout.ts
+++ frontend/src/lib/logout.ts
@@ -1,7 +1,9 @@
+import { API_BASE_URL } from "./config";
+
 export async function logout() {
   localStorage.removeItem('token');
   localStorage.removeItem('user');
-  await fetch(`http://localhost:8080/api/logout`, {
+  await fetch(`http://${API_BASE_URL}/api/logout`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     credentials: 'include',
