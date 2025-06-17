09e52b9e0a69649b231e93753fdce2f50f239757
bzelaznicki <bartosz@zelaznicki.com>
2025-06-17 15:34:59 +0200

    bugfix: added missing package items

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,7 +4,7 @@ import Layout from '@/components/Layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Toaster } from 'react-hot-toast';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
