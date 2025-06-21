e51c2798866639a4241f6f80428ae2794821ca21
bzelaznicki <bartosz@zelaznicki.com>
2025-06-21 23:27:55 +0200

    BZC-112 Added create product endpoint

--- backend/router_api_admin.go
+++ backend/router_api_admin.go
@@ -8,5 +8,6 @@ import (
 func (cfg *apiConfig) registerApiAdminRoutes(mux *http.ServeMux) {
 	log.Printf("Registering Shop API routes...")
 	mux.Handle("GET /api/admin/products", cfg.checkAuth(cfg.checkAdmin(http.HandlerFunc(cfg.handleApiAdminGetProducts))))
+	mux.Handle("POST /api/admin/products", cfg.checkAuth(cfg.checkAdmin(http.HandlerFunc(cfg.handleApiCreateProduct))))
 	log.Printf("Shop API routes registered")
 }
